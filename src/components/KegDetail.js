4f5b979a3a869dff9f416615741fb6fb1803a468
acolistro <arcolistro@gmail.com>
2020-05-02 21:03:17 -0700

    add buttons to keg detail.

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from "prop-types";
 
 function KegDetail(props) {
-  const { keg } = props;
+  const { keg, onClickingDelete } = props;
 
   return (
     <React.Fragment>
@@ -10,6 +10,8 @@ function KegDetail(props) {
       <h3>{keg.beer} - {keg.dateTapped}</h3>
       <h3>{keg.brand} - {keg.style} - {keg.pricePerPint} - {keg.alcoholContent}</h3>
       <h4><em>{keg.pintsLeft}</em></h4>
+      <button onClick={ props.onClickingEdit }>Update Keg</button>
+      <button onClick={()=> onClickingDelete(keg.id) }>Delete Keg</button>
  
       {/* <form onClick={handlePintSold}>
         <button onClick={ props.onPintSold }>Pint Sold</button>
